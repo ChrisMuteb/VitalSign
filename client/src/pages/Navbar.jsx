0c16a6dae02be452f90c9ab386067756ee94526b
Anh Vu Bui <buianhvu190999@gmail.com>
2024-01-24 00:10:14 +0100

    revert to HEAD

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -1,20 +1,10 @@
-import React from "react";
-import { GiHeartPlus } from "react-icons/gi";
 const Navbar = () => {
-
-
     return (
-        <nav className="navbar w-full flex fixed h-20 bg-slate-100 px-4 items-center justify-between text-blue-600">
-            <div className="flex">
-                <GiHeartPlus className="text-red-600 w-10 h-10"/>
-                <h1 className="text-2xl">VitalSign</h1>
-            </div>
+        <nav className="navbar">
+            <h1>VitalSign</h1>
             <div className="links">
-                <div className="flex items-center space-x-4">
-                    <a href="/" className="bg-blue-600 text-white w-40 text-center">Vous êtes practicien?</a>
-                    <a href="/create" className="bg-blue-600 text-white w-40 text-center">Se Connecter</a>
-
-                </div>
+                <a href="/">Etes vous? Practicien?</a>
+                <a href="/create">Se connecter</a>
             </div>
         </nav>
     );
