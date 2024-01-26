47d8a9e43f55b27b7b13e72eb4b4c62244875695
chriskayomb <ibond.chris@gmail.com>
2024-01-26 18:28:05 +0100

    home page completed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
+
 import React from "react";
+import SearchBar from "./SearchBar";
 import Navbar from "./Navbar";
 
 const Home = () => {
     return (
         <div className="home">
             <Navbar />
-            <h1>Home Page</h1>
+            <SearchBar />
         </div>
     )
 }
