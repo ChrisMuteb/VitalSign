36edfb8c7d3607e0f143c23b9d9550de1a4a005d
chriskayomb <ibond.chris@gmail.com>
2024-01-31 17:38:44 +0100

    Chatbot server side completed

--- server/services/ai.service.js
+++ server/services/ai.service.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const OpenAI = require("openai");
+require('dotenv').config();
+
+const openai = new OpenAI({
+    apiKey: process.env.OPEN_AI_KEY
+});
 
 const messageChatbot = async (req, res) => {
     const { message } = req.body;
-    console.log('User: ', message);
-    res.status(200).send({ response: 'chat said Hi' });
+    const response = await openai.chat.completions.create({
+        model: 'gpt-3.5-turbo',
+        messages: [{ "role": "user", "content": message }],
+        max_tokens: 100
+    })
+    res.status(200).send({ ai: response.choices[0].message.content })
 }
 
 module.exports = { messageChatbot }
